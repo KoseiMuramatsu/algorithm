3c2ca414a539599d476593475d23e4dd1dcde99b
KoseiMuramatsu <https://github.com/KoseiMuramatsu>
2020-05-25 16:15:10 +0900

    97b回答

---
+++ 97b.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var x int
+	fmt.Scan(&x)
+	ans := 1
+	// 全探索
+	for i := 2; i <= x; i++ {
+		for j := 2; j <= x; j++ {
+			num := int(math.Pow(float64(i), float64(j)))
+			if num > x {
+				// 2の4乗でbreakになって、次のi := 3がはじまる。
+				// iが10になるまでやる
+				break
+			} else if num > ans {
+				ans = num
+			}
+		}
+	}
+	fmt.Println(ans)
+}
+
+// package main
+// import "fmt"
+
+// func main(){
+
+// 	for i := 0; i < 5; i++ {
+
+// break文使うと処理を中断してそのブロックを抜け出すらしい。
+
+// 		if i == 3 {
+// 			break
+// 		}
+// 		fmt.Println(i); // 0,1,2が出力される
+// 	}
+// }
