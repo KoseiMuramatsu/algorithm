dd65c21fcadf8e1ac3c01350c1cad9e10ee040ba
KoseiMuramatsu <https://github.com/KoseiMuramatsu>
2020-06-03 16:25:56 +0900

    99b-2回答

---
+++ 99b-2.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+	diff := b - a
+	west := 0
+	// 結局これは等差数列の和
+	for i := 1; i < diff; i++ {
+		west += i
+	}
+	fmt.Println(west - a)
+}
+
+// 6/3 ✕ おしい。
+// 解き方は理解した。
