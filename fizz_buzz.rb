a6d1ff328b4629447bc58afde7d869371e1798cb
KoseiMuramatsu <https://github.com/KoseiMuramatsu>
2020-07-15 17:43:27 +0900

    fizz_buzz

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,13 +1,17 @@
-for num in 1..100 do
-  if (num % 3 == 0 && num % 5 == 0)
-    puts "fizzBuzz"
-  elsif num % 3 == 0
-    puts "Fizz"
-  elsif num % 5 == 0
-    puts "Buzz"
-  else
-    puts num
-  end
+# for num in 1..100 do
+#   if (num % 3 == 0 && num % 5 == 0)
+#     puts "fizzBuzz"
+#   elsif num % 3 == 0
+#     puts "Fizz"
+#   elsif num % 5 == 0
+#     puts "Buzz"
+#   else
+#     puts num
+#   end
+# end
+
+def main(lines)
+  puts 1
 end
 
-
+main(readlines)
